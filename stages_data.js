bd9dd70ac579ca043778fe0dce19b900854faa2e
Arlo Belshee <abelshee>
2017-04-12 17:05:48 -0700

    Extracted the layout CSS & JS from the main page. Added an explicit file format version number to the file (in addition to the content version that was already there).

--- stages_data.js
+++ stages_data.js
@@ -1,6 +1,7 @@
-function stages_data() {
+function load_data() {
 	return {
 		"version": "0.11.0",
+		"file_format_version": "2.0.0",
 		"levels": {
 			"L_TRADITIONAL": {
 				"name": "Traditional done well",
